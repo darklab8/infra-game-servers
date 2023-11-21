d0baaf62af88599a0c9b0b461e21c9b2ac2b0a17
dd84ai <dd84ai@gmail.com>
2023-11-21 15:15:06 +0100

    feat: add Commandantt pinging

--- bot/avorioner/main.go
+++ bot/avorioner/main.go
@@ -73,6 +73,9 @@ func reactToEvent(line string) {
 		} else if strings.Contains(shipname, "CPM-") {
 			logus.Info("Recognized as couden's ship")
 			dg.SendDeduplicatedMsg(dedup, fmt.Sprintf("<@328587807071272970> %s", msg), avorioner_settings.OthersChannel)
+		} else if strings.Contains(shipname, "COM-") {
+			logus.Info("Recognized as Сommandantt's ship")
+			dg.SendDeduplicatedMsg(dedup, fmt.Sprintf("<@297460626576506881> %s", msg), avorioner_settings.OthersChannel)
 		} else if strings.Contains(shipname, "DARK-") {
 			logus.Info("Recognized as darkwind's ship")
 			dg.SendDeduplicatedMsg(dedup, fmt.Sprintf("<@370435997974134785> %s", msg), avorioner_settings.DarkwindChannel)
