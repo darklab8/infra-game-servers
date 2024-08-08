0a9b0d5fae6610940e1188436c8dbffab1f72ac6
dd84ai <dd84ai@gmail.com>
2024-08-09 00:51:40 +0200

    fix: turn off galaxyspace stuff

--- tf/production/minecraft.tf
+++ tf/production/minecraft.tf
@@ -25,7 +25,7 @@ provider "docker" {
 
 resource "docker_image" "minecraft" {
   provider = docker.minecraft
-  name     = "darkwind8/minecraft:modded-1.7.10-v0.4.2"
+  name     = "darkwind8/minecraft:modded-1.7.10-v0.4.3"
   # keep_locally = true
 }
 
