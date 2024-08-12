66e461b5204a8243e0d0b50f5bffc28c9eb530eb
dd84ai <dd84ai@gmail.com>
2024-08-12 02:25:01 +0200

    fix: add longer timeouts

--- tf/production/minecraft.tf
+++ tf/production/minecraft.tf
@@ -25,7 +25,7 @@ provider "docker" {
 
 resource "docker_image" "minecraft" {
   provider = docker.minecraft
-  name     = "darkwind8/minecraft:modded-1.7.10-v0.4.4"
+  name     = "darkwind8/minecraft:modded-1.7.10-v0.4.5"
   # keep_locally = true
 }
 
